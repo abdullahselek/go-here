bec456248d80c7e09b1a46ba02314868cad63511
Abdullah Selek <abdullahselek@gmail.com>
2019-08-09 19:32:43 +0100

    Reverse geocode for landmarks.

--- here/reverse_geocoding.go
+++ here/reverse_geocoding.go
@@ -24,6 +24,15 @@ type AddressFromLocationParameters struct {
 	AppCode    string `url:"app_code"`
 }
 
+// LandmarksParameters used for fetching address from location.
+type LandmarksParameters struct {
+	Prox    string `url:"prox"`
+	Mode    string `url:"mode"`
+	Gen     int    `url:"gen"`
+	AppID   string `url:"app_id"`
+	AppCode string `url:"app_code"`
+}
+
 // newReverseGeocodingService returns a new GeocodingService.
 func newReverseGeocodingService(sling *sling.Sling, appID string, appCode string) *ReverseGeocodingService {
 	return &ReverseGeocodingService{
@@ -44,5 +53,13 @@ func (s *ReverseGeocodingService) AddressFromLocation(latlong [2]float32, diamet
 	addressFromLocationParameters := &AddressFromLocationParameters{Prox: createProx(latlong, diameter), Mode: ReverseGeocodingMode.String(mode), MaxResults: maxResults, Gen: gen, AppID: s.AppID, AppCode: s.AppCode}
 	geocodingResponse := new(GeocodingResponse)
 	resp, err := s.sling.New().Get("reversegeocode.json").QueryStruct(addressFromLocationParameters).ReceiveSuccess(geocodingResponse)
+	return geocodingResponse, resp, err
+}
+
+// Landmarks returns details of landmarks near to a given latitude and longitude.
+func (s *ReverseGeocodingService) Landmarks(latlong [2]float32, diameter int, gen int) (*GeocodingResponse, *http.Response, error) {
+	landmarksParameters := &LandmarksParameters{Prox: createProx(latlong, diameter), Mode: ReverseGeocodingMode.String(RetrieveLandmarks), Gen: gen, AppID: s.AppID, AppCode: s.AppCode}
+	geocodingResponse := new(GeocodingResponse)
+	resp, err := s.sling.New().Get("reversegeocode.json").QueryStruct(landmarksParameters).ReceiveSuccess(geocodingResponse)
 	return geocodingResponse, resp, err
 }
--- here/reverse_geocoding_test.go
+++ here/reverse_geocoding_test.go
@@ -19,3 +19,16 @@ func TestReverseGeocodingService_AddressFromLocation(t *testing.T) {
 	assert.NotNil(t, geocodingResponse)
 	assert.Nil(t, err)
 }
+
+func TestReverseGeocodingService_Landmarks(t *testing.T) {
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(readJSONFile("resources/reverse_geocoding_landmarks_response.json"))
+	})
+	httpClient, teardown := testingHTTPClient(h)
+	defer teardown()
+
+	client := NewReverseGeocodingClient(httpClient, "appID", "appCode")
+	geocodingResponse, _, err := client.ReverseGeocoding.Landmarks([2]float32{42.3902, -71.1293}, 1, 9)
+	assert.NotNil(t, geocodingResponse)
+	assert.Nil(t, err)
+}
